4bc2c06d32ba36a5eea9462352217bef1ec08c82
loreiom <75540866+loreiom@users.noreply.github.com>
2022-11-11 21:37:36 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+function Start(){
+    text = prompt("Enter Text")
+  count = prompt("Enter count")
+    for (let i = 0; i < count; i++) {
+      document.write("<br>",i + 1,".", " ", text)
+      document.write()
+    }
+  
+   }
+  
